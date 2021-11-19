c7d6f6684d36b3000f9fad8aeffbe9586cc11fcd
sang <dsang1113@gmail.con>
2021-11-19 16:25:15 +0700

    xoa tinh tich cua user2 va lam tinh tich

--- source.cpp
+++ source.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
-int tong(int a, int b);
 int hieu(int a, int b){
 	return a-b;
 };
-=======
 int tong(int a, int b){
 	return a+b;
 };
-int hieu(int a, int b);
->>>>>>> refs/remotes/origin/main
+int tich(int a, int b){
+	return a*b;
+};
+
 
 int main()
 {
@@ -20,15 +19,14 @@ int main()
 	cin >> a;
 	cout << "Nhap so nguyen 2: ";
 	cin >> b;
-	cout << "Chon phep toan (+,-) ";
+	cout << "Chon phep toan (+,-,*) ";
 	cin >> phepToan;
-<<<<<<< HEAD
 	if(phepToan == '-')
 		cout<<"Hieu la: "<<hieu(a,b)<<endl;
-=======
 	if(phepToan == '+')
 		cout<<"Tong la: "<<tong(a,b)<<endl;
->>>>>>> refs/remotes/origin/main
+	if(phepToan == '*')
+		cout<<"Tich la: "<<tich(a,b)<<endl;
 	system("pause");
 	return 0;
 }
